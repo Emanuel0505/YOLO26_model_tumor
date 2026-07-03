e9eef12ed7145bc70d6f820bf37a212cf0e4454d
Emanuel João <163889723+Emanuel0505@users.noreply.github.com>
2026-07-03 15:51:54 -0300

    refactor: refaÃtoração e organização do codigo para arquivos .py do .ipynb

---
+++ setup/GLOBAL.py
@@ -0,0 +1,17 @@
+import os
+import kagglehub
+
+DATASET_ORINGIN = kagglehub.dataset_download("indk214/brain-tumor-dataset-segmentation-and-classification")
+
+YOLO_DATASET = 'brain-tumor-dataset-YOLO'
+
+CLASS = {'glioma': 0,  'meningioma': 1, 'pituitary': 2}
+
+SPLIT_VAL_AND_TEST = 0.3
+SPLIT_TEST = 0.1
+
+PATH_YAML = os.path.join(YOLO_DATASET, 'data.yaml')
+
+DRIVE = False 
+
+path_drive = '/content/drive/MyDrive/visaocomputacional/Projeto-Deteccao_e_Tumor_no_Cerebro' if DRIVE else 'runs/train'
---
+++ setup/model-train.py
@@ -0,0 +1,16 @@
+from GLOBAL import *
+from sklearn.model_selection import train_test_split
+from ultralytics import YOLO
+import matplotlib.pyplot as plt
+import os
+
+model = YOLO("yolo26n-seg.pt")
+
+model.train(
+    data=PATH_YAML,
+    epochs=100,
+    plots=True,  
+    device=0,
+    name= 'Model_tumor_yolo',
+    project=PATH_MODEL_TRAIN,
+)
