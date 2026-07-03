6a107c4cc929beb3951b1039572e4895299bb41d
Emanuel João <163889723+Emanuel0505@users.noreply.github.com>
2026-07-03 15:54:07 -0300

    feat: scripts para rodar os arquivos .py  em supercomputadores com Slurm Workload Manager

---
+++ scripts/script-program copy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH --partition=gpu-4-a100
+#SBATCH --gpus-per-node=1
+#SBATCH --cpus-per-task=6
+#SBATCH --mem=32G
+#SBATCH --time=-03:00
+#SBATCH --output=slurm-%j.out
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+
+python ../setup/model-train.py
