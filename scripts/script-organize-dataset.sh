bc300992676e4290568399f2ee08300c89d2ca73
Emanuel João <163889723+Emanuel0505@users.noreply.github.com>
2026-07-06 22:41:59 -0300

    bug: arrumando  sintex de tempo e caminho

--- scripts/script-organize-dataset.sh
+++ scripts/script-organize-dataset.sh
@@ -3,8 +3,8 @@
 #SBATCH --gpus-per-node=1
 #SBATCH --mem=32G
 #SBATCH --cpus-per-task=6
-#SBATCH --time=-03:00
+#SBATCH --time=0-00:10
 #SBATCH --output=slurm-%j.out
 export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
 
-python ../setup/organize-dataset.py
+python  ../setup/organize-dataset.py
--- scripts/script-train-model.sh
+++ scripts/script-train-model.sh
@@ -3,7 +3,7 @@
 #SBATCH --gpus-per-node=1
 #SBATCH --cpus-per-task=6
 #SBATCH --mem=32G
-#SBATCH --time=-03:00
+#SBATCH --time=0-03:00
 #SBATCH --output=slurm-%j.out
 export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
 
